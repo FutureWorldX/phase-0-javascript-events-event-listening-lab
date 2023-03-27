ff1575c9b9e3b2bb2118f3cfde0c445b96733f70
Future <futureworldzone@gmail.com>
2023-03-27 14:47:41 +0300

    Completed assignment with required changes from the README.md file on the lab for JavaScript Event Listening

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 function addingEventListener() {
+    //binds an event listener in addingEventListener():
+    input.addEventListener('click', clickAlert);
+    //basically binding is including an event using the JavaScript syntax written above
 }
+
+//code is available from the README.md file
+const input = document.getElementById('input');
+
+function clickAlert() {
+  alert('I was clicked!');
+}
+
+input.addEventListener('click', clickAlert);
